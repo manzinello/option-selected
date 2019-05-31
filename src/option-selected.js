370f1ff100f549248f88a1ed401f33d470bbfd0a
Matteo Manzinello <matteo@manzinello.com>
2019-05-31 17:15:14 +0200

    for let instead forEach

--- src/option-selected.js
+++ src/option-selected.js
@@ -1,18 +1,18 @@
 const optionSelected = () => {
   let selects = document.getElementsByTagName("select");
 
-  selects.forEach(select => {
+  for (let select of selects) {
     if (select.value != "") {
       let options = select.getElementsByTagName("option");
-      options.forEach(option => {
+      for (let option of options) {
         if (select.value === option.value) {
           option.selected = true;
         } else {
           option.selected = false;
         }
-      });
+      }
     }
-  });
+  }
 };
 
 document.addEventListener("DOMContentLoaded", optionSelected);
